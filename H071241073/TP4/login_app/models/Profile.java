6f4bcea8139ad672bbb362eb6c5f18c08c087e9b
Davidzen <davidzen662@gmail.com>
2025-05-27 13:27:03 +0800

    Pengumpulan Tugas Praktikum 4

---
+++ H071241073/TP4/login_app/models/Profile.java
@@ -0,0 +1,54 @@
+package login_app.models;
+
+public class Profile {
+    /*
+     * TODO:
+     * Tambahkan attribute, method, atau constructor
+     * yang dibutuhkan di kelas user
+     */
+    private String fullName;
+    private String nickName;
+    private int age;
+    private String hobby;
+    private String gender;
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+}
